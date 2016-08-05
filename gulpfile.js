20a7f7e23dd0b50cf929e75a8ecda139d8b7b98a
杨波 <hzyangbo15@corp.netease.com>
2016-08-05 15:50:51 +0800

    add: 新增开发分支，仅仅只是为了熟练git命令

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+    concat = require('concat'),
+    del = require('del'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    sourcemaps = require('gulp-sourcemaps'),
+    clean = require('gulp-clean'),
+    babel = require('babel'),
+    livereload = require('livereload');
+
+//start 
